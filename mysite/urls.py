09f0e8db2e4ba9212186fd8cc7341994e0e10299
pythonanywhere <your_email@example.com>
2021-10-09 15:27:40 +0000

    css

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
+    path('', views.home, name="home"),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
